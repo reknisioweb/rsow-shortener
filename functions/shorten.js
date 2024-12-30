6c238b8dff9be139e232cba3005fd58bcf73b92e
Martin Kopta <martin.kopta@garcon.cz>
2024-12-30 01:58:27 +0100

    Update

--- functions/shorten.js
+++ functions/shorten.js
@@ -2,21 +2,22 @@ const urls = {
   "ex1": "https://www.example.com",
   "ex2": "https://www.example.org",
   "ex3": "https://www.example.com/?pokus",
-  "ex4": "https://www.example.com"
+  "ex4": "https://www.example.com",
 };
 
 exports.handler = async (event) => {
-  const short = event.queryStringParameters.short;
-  const longUrl = urls[short];
+  // Extrahujeme část cesty za lomítkem
+  const path = event.path.replace("/.netlify/functions/shorten", "").replace("/", "");
+  const longUrl = urls[path];
 
   if (longUrl) {
     return {
       statusCode: 301,
       headers: {
-        Location: longUrl, // Cílová URL
-        "Cache-Control": "no-store", // Zajistí, že prohlížeč nebude uchovávat staré hodnoty
+        Location: longUrl, // Přesměrování na cílovou URL
+        "Cache-Control": "no-store", // Zabrání cacheování
       },
-      body: null, // Tělo není potřeba pro přesměrování
+      body: null, // Tělo odpovědi není potřeba
     };
   }
 
