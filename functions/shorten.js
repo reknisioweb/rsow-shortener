0d7e9a357cfe26d1c172a4727a21574731bf880a
Martin Kopta <martin.kopta@garcon.cz>
2024-12-30 01:33:47 +0100

    Update

--- functions/shorten.js
+++ functions/shorten.js
@@ -1,23 +1,25 @@
 const urls = {
-    "ex1": "https://www.example.com",
-    "ex2": "https://www.example.org",
-  };
-  
-  exports.handler = async (event) => {
-    const short = event.queryStringParameters.short;
-    const longUrl = urls[short];
-  
-    if (longUrl) {
-      return {
-        statusCode: 301,
-        headers: {
-          Location: longUrl,
-        },
-      };
-    }
-  
+  "ex1": "https://www.example.com",
+  "ex2": "https://www.example.org",
+};
+
+exports.handler = async (event) => {
+  const short = event.queryStringParameters.short;
+  const longUrl = urls[short];
+
+  if (longUrl) {
     return {
-      statusCode: 404,
-      body: "URL not found",
+      statusCode: 301,
+      headers: {
+        Location: longUrl, // Cílová URL
+        "Cache-Control": "no-store", // Zajistí, že prohlížeč nebude uchovávat staré hodnoty
+      },a
+      body: null, // Tělo není potřeba pro přesměrování
     };
+  }
+
+  return {
+    statusCode: 404,
+    body: "URL not found",
   };
+};
