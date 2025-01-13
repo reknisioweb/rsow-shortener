696bbe322dcee610be7e46375aa23651efce22ef
Martin Kopta <martin.kopta@garcon.cz>
2025-01-13 04:53:22 +0100

    Update

--- functions/redirect.js
+++ functions/redirect.js
@@ -1,9 +1,11 @@
+// Load environment variables from a .env file if not in production
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 
 const mongoose = require('mongoose');
 
+// Get the MongoDB URI from environment variables
 const mongoUri = process.env.MONGO_URI;
 if (!mongoUri) {
     throw new Error('MONGO_URI is not defined');
@@ -11,21 +13,27 @@ if (!mongoUri) {
 
 let connection = null;
 
+// Define the schema for the URL model
 const urlSchema = new mongoose.Schema({
-  shortId: { type: String, required: true, unique: true },
-  originalUrl: { type: String, required: true },
+  shortId: { type: String, required: true, unique: true }, // Unique identifier for the shortened URL
+  originalUrl: { type: String, required: true }, // The original URL to redirect to
 });
 
-// Pokud ještě neexistuje model.Url, vytvoříme ho.
+// Create the URL model if it doesn't already exist
 const Url = mongoose.models.Url || mongoose.model('Url', urlSchema);
 
+/**
+ * Connects to the MongoDB database if not already connected.
+ * @throws Will throw an error if the database connection fails.
+ */
 async function connectToDatabase() {
   if (!connection) {
     try {
+      // Connect to the MongoDB database
       connection = await mongoose.connect(mongoUri, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        dbName: 'rsow-shortener',
+        dbName: 'rsow-shortener', // Specify the database name
       });
     } catch (error) {
       console.error('Database connection error:', error);
@@ -34,6 +42,11 @@ async function connectToDatabase() {
   }
 }
 
+/**
+ * Handler function to redirect short URLs to their original URLs.
+ * @param {Object} event - The event object containing request details.
+ * @returns {Object} - The response object with status code and headers.
+ */
 exports.handler = async (event) => {
   let shortId;
 
